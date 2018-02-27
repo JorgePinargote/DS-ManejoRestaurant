6dced9b84f7be3c283dbb175fade07d3141cf65f
JorgePinargote <jorge.apif@gmail.com>
2018-02-27 10:36:57 -0500

    reporte de test de pedidos

---
+++ ProyectoDS/test/testPrecio.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Jorge Pinargote
+ */
+public class testPrecio extends TestCase {
+    
+    public void testEqual() {
+        assertEquals(1,1);
+    }
+    
+    
+}
